323c9964ef7148d121034a908ff1dcc63ca99343
tshamsiddin <tokhirjonovshamsiddin@gmail.com>
2022-04-15 23:56:56 +0900

    first commit

---
+++ src/Containers/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import CardList from "../Components/CardList";
+import SearchBox from '../Components/SearchBox';
+import Scroll from "../Components/Scroll";
+import './App.css';
+
+class App extends React.Component{
+    constructor() {
+        super()
+        this.state={
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(users=>this.setState({robots:users}));
+    }
+
+
+
+    onSearchChange=(e)=> {
+        this.setState({searchfield: e.target.value})
+    }
+
+
+
+    render() {
+        const filteredRobots=this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        if (this.state.robots.length===0){
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                    <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
+        
+    }
+} 
+export default App;
